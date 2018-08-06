24017ef4110d7d35ac400a8987f38995e21bd28c
max1Q <41213048+max1Q@users.noreply.github.com>
2018-08-06 17:34:44 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,7 +1,8 @@
 node default {
   file { '/root/README':
     ensure => file,
-    content => 'Test first line',
-    owner => 'root',
-  }  
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
+
