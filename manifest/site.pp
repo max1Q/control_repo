6461b3f838da9f7a968df2338778ec9e1b3c98f8
max1Q <41213048+max1Q@users.noreply.github.com>
2018-08-06 18:08:15 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/TSTfile':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-  file {'/tmp/test1.log':
-    ensure => file,
-    content => 'Hi Dude!!!"
-  }
+    file {'/root/TSTfile':
+        ensure => file,
+        content => 'This is a readme',
+        owner   => 'root',
+    }
+        file {'/tmp/test1.log':
+        ensure => file,
+        content => 'Hi Dude!!!"
+    }
 }
 
